d184f23eda9b14d9fc2138c7097c7d7318e9b5d7
Suman Boro <borosuman@gmail.com>
2018-03-15 18:42:17 +0530

    Added Periodic task

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 from flask import Flask
 from celery import Celery
 from celery.schedules import crontab
+from celery.utils.log import get_task_logger
 
 app = Flask(__name__)
-
-CELERY_IMPORTS = ('app.tasks.test')
-CELERY_TASK_RESULT_EXPIRES = 30
-CELERY_TIMEZONE = 'UTC'
-
-CELERY_ACCEPT_CONTENT = ['json', 'msgpack', 'yaml']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
-CELERYBEAT_SCHEDULE = {
-    'test-celery': {
-        'task': 'app.tasks.test.print_hello',
-        # Every minute
-        'schedule': crontab(minute="*"),
-    }
-}
+logger = get_task_logger(__name__)
 
 def make_celery(app):
-
     #Celery configuration
     app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
     app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+    app.config['CELERYBEAT_SCHEDULE'] = {
+        # Executes every minute
+        'periodic_task-every-minute': {
+            'task': 'periodic_task',
+            'schedule': crontab(minute="*")
+        }
+    }
 
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
@@ -43,10 +35,10 @@ celery = make_celery(app)
 def view():
     return "Hello, Flask is up and running!"
 
-@celery.task()
-def print_hello():
-    logger = print_hello.get_logger()
-logger.info("Hello")
+@celery.task(name ="periodic_task")
+def periodic_task():
+    print('Hi! from periodic_task')
+    logger.info("Hello! from periodic task")
 
 if __name__ == "__main__":
     app.run(debug = True)
